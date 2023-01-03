39022cc2c9191e98ce2a90a5352e10d2040fe62c
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-01-04 00:51:14 +0530

    Update Combination_sum_iii.cpp

--- Microsoft/2. Combination Sum iii/Combination_sum_iii.cpp
+++ Microsoft/2. Combination Sum iii/Combination_sum_iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // recursion using pick / non-pick technique
     vector<vector<int>> ans;
     void solve(vector<int> arr,int size,vector<int> res,int index,int target){
         if(size==0 && target==0){
@@ -8,12 +9,15 @@ public:
         }
         if(target<0 || size==0 || index>=arr.size())
             return;
+        //take
         res.push_back(arr[index]);
         solve(arr,size-1,res,index+1,target-arr[index]);
         res.pop_back();
+        //not take
         solve(arr,size,res,index+1,target);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
+        // tak an array from 1-9 and choose the target sum of n from k elements.
         vector<int> arr={1,2,3,4,5,6,7,8,9};
         vector<int> res;
         solve(arr,k,res,0,n);
