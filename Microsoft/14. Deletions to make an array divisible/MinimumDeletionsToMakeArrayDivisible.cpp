3454ea4386293ed406b532ffc0d244d75d9878cc
Namrata <74545248+namratabose32@users.noreply.github.com>
2023-01-08 01:33:25 +0530

    Update MinimumDeletionsToMakeArrayDivisible.cpp

--- Microsoft/14. Deletions to make an array divisible/MinimumDeletionsToMakeArrayDivisible.cpp
+++ Microsoft/14. Deletions to make an array divisible/MinimumDeletionsToMakeArrayDivisible.cpp
@@ -1,3 +1,5 @@
+// Method 1: TC:O(nums*numsDivide) + O(log nums)
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
@@ -23,3 +25,35 @@ public:
     }
 };
 // hindi me comments dalne ke liye khed h........ pr mazzzzaaaaaaaa ayaaaaaaa :) 
+
+
+//method 2 : using gcd  -------> TC: O(N)+O(log N)
+
+class Solution {
+public:
+    int gcd(int a,int b){
+        if(b==0)
+            return a;
+        return gcd(b,a%b);
+    }
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        int ans=0;
+        map<int,int> mp;
+        for(auto x:nums)
+            mp[x]++;
+        // find greatest common divisor of numsDivide so as to easily find 1st element from nums which can divide all the elements of numsDivide
+        int g=numsDivide[0];
+        for(auto x:numsDivide){
+            g=gcd(g,x);
+        }
+        // since map sorts all the elements so we can easily find the sortest element and time complexity can decrease a little bit if there are repitation of elements which are non-divisible
+        for(auto x:mp){
+            if(g%x.first==0){
+                return ans;
+            }
+            else{
+                ans+=x.second;
+            }
+        }return -1;
+    }
+};
